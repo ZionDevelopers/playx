308f7708bf216b597c93953eee8f175854a16edc
Júlio C. Oliveira <talk@juliocesar.me>
2016-07-13 13:31:12 -0300

    Update

--- js/init.js
+++ js/init.js
@@ -16,16 +16,14 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-43230779-1', 'ziondevelopers.github.io');
 ga('send', 'pageview'); 
-
-
+ 
 var si = 0;
 
 $(document).ready(function () {
-	setInterval("$('#botr_umB5Dfyh_IJzySFh8_div').remove();", 200);
+	si = setInterval("$('div#botr_umB5Dfyh_IJzySFh8_div').remove();", 200);
 });
-
-window.onload = function () { 
-
+ 
+window.onload = function () {  
 	/** Initialize player **/
 	jwplayer("player").setup({
 	  "aspectratio": "16:9",
