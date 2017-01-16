aadc336a93caf9a78006f3290fdd6bc77b9c1596
Júlio C. Oliveira <talk@juliocesar.me>
2017-01-15 23:34:07 -0200

    Twitch init update

---
+++ js/twitch.js
@@ -0,0 +1,44 @@
+function get(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43230779-1', 'ziondevelopers.github.io');
+ga('send', 'pageview'); 
+
+$(document).ready(function () {
+	var channel = "twitch";
+	var video = 0;
+	var options = {
+		width: window.innerWidth,
+		height: window.innerHeight,
+		channel: "twitch",		
+	};
+	
+	channel = $.trim(unescape(get("channel")));	
+	video = parseInt($.trim(unescape(get("video"))));
+	console.log("Channel: " + channel + ", video:" + video);
+	
+	if (channel != "" && channel != 0) {
+		options.channel = channel;
+	}
+	
+	if (video !== 0) {
+		options.video = video;
+	}
+	
+	if (channel != "" ) {
+		window.player = new Twitch.Player("div#player", options);
+	}
+});
