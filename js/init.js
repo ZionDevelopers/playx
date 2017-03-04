35ce6afa6a5ad90e08c23a0d75c964e415a7c4df
Adam759 <adam759@comcast.net>
2017-03-04 03:54:13 -0600

    Old Google Analytics Re-enabled

--- js/init.js
+++ js/init.js
@@ -19,17 +19,16 @@ function getByURL(variable, query)
        return(false);
 }
 
-//Google Analytics -- OLD
-/*
+//Google Analytics - Zion
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-43230779-1', 'ziondevelopers.github.io'); //Zion Tracker
-ga('create', 'UA-92533032-1', 'ziondevelopers.github.io', 'PlayX Hosting'); //Science Tracker
 ga('send', 'pageview'); 
-*/
+
 
 jwplayer.key = "lmwviL3c55Ymnx4fMjEUQeiU00zeXf6TCiDHQA==";
 
