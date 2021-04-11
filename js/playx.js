b47ceef81723056346ed9e217227d1b665f1d645
Júlio C. Oliveira <talk@juliocesar.me>
2021-04-11 18:57:45 -0300

    Updated google analytics trackes to a working new version

--- js/playx.js
+++ js/playx.js
@@ -255,16 +255,3 @@ var JWPlayer = function(videoId, startoutput){
 			YouTubeIFramePlayer(videoId, startoutput);
 		}
 	}
-	
-//------------------------------------------------- ANALYTICS -------------------------------------------------//
-	
-  //Google Analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	
-  ga('create', 'UA-92533032-1', 'auto'); //Science Tracker
-  ga('create', 'UA-43230779-1', 'ziondevelopers.github.io'); //Dathus Tracker
-  ga('send', 'pageview');
-  ga('ziondevelopers.github.io', 'pageview');
