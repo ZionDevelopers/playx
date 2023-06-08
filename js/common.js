782d968f7942e274961caa4787aabba8e64aae82
Júlio C. Oliveira <talk@juliocesar.me>
2023-06-08 09:08:15 -0300

    Better formatting and better code reuse

--- js/common.js
+++ js/common.js
@@ -1,10 +1,12 @@
 //Get a var from URL
-function get(variable){
+function get(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split("&");
     for (var i=0;i<vars.length;i++) {
         var pair = vars[i].split("=");
-            if(pair[0] == variable){return pair[1];}
+            if (pair[0] == variable) {
+                return pair[1];
+            }
         }
     return(false);
 }
@@ -13,9 +15,10 @@ function get(variable){
 function getByURL(variable, query){
     var vars = query.split("?");
     for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            if(pair[0] == variable){return pair[1];
-    }
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
     }
     return(false);
 }
