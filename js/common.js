6480e7952f14266b6c974b9c5b1f2bf733a8cdc9
Júlio C. Oliveira <talk@juliocesar.me>
2023-06-08 08:56:10 -0300

    Added a common js and a tracking js to be shared
    To be shared on all html files.

---
+++ js/common.js
@@ -0,0 +1,29 @@
+//Get a var from URL
+function get(variable){
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+        }
+    return(false);
+}
+
+//Get a param from a pure URL Format, so pull a param from a pure ?url= param
+function getByURL(variable, query){
+    var vars = query.split("?");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];
+    }
+    }
+    return(false);
+}
+
+//Look At URL For Params
+var urlParam = function(name, w){
+    w = w || window;
+    var rx = new RegExp('[\&|\?]'+name+'=([^\&\#]+)'),
+    val = w.location.search.match(rx);
+    return !val ? '':val[1];
+}
