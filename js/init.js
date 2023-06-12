7ce49483e2369f0039b2cabc62d1b22a18ff1a35
Júlio C. Oliveira <talk@juliocesar.me>
2023-06-12 11:26:11 -0300

    updated host_v2 to work with youtube

--- js/init.js
+++ js/init.js
@@ -3,29 +3,53 @@ jwplayer.key = "lmwviL3c55Ymnx4fMjEUQeiU00zeXf6TCiDHQA==";
 // Get URL
 var url = unescape(get("url"));
 
-jwplayer.pause = jwplayer.pauseVideo
-jwplayer.play = jwplayer.playVideo
+// Youtube API Fallback
+
+window.onYouTubeIframeAPIReady = function () {
+    window.jwplayer = new YT.Player("player", {
+        "width": window.innerWidth,
+        "height": window.innerHeight,
+        videoId: getByURL("v", url),
+        playerVars: {
+            controls: 0,
+            autoplay: 1,
+            showinfo: 0,
+            iv_load_policy: 3,
+            autohide: 0,
+            rel: "0",
+            wmode: "opaque",
+            modestbranding: 1,
+            //nohtml5: 1
+        }
+    });
+
+    jwplayer.pause = jwplayer.pauseVideo
+    jwplayer.play = jwplayer.playVideo
+}
 
 $(document).ready(function () {
     // Check for URL
-    if (url != "false") {    
-        /** Initialize player **/
-        jwplayer("player").setup({
-            "aspectratio": "auto",
-            "autostart": true,
-            "controls": true,
-            "displaydescription": false,
-            "displaytitle": true,
-            "file": url,
-            "preload": "auto",
-            "primary": "flash", // Use HTML5 As Primary To Have Native YouTube WebM HTML5 Support, Flash Fallback For Other Content
-            "repeat": false,
-            "loop": false,
-            "stagevideo": false,
-            "stretching": "uniform",
-            "visualplaylist": false,
-            "width": window.innerWidth,
-            "height": window.innerHeight
-        });        
+    if (url != "false") {
+        // Uncomment For Youtube API Fallack
+        if (url.indexOf("youtube.com") === -1) {
+            /** Initialize player **/
+            jwplayer("player").setup({
+                "aspectratio": "auto",
+                "autostart": true,
+                "controls": true,
+                "displaydescription": false,
+                "displaytitle": true,
+                "file": url,
+                "preload": "auto",
+                "primary": "flash", // Use HTML5 As Primary To Have Native YouTube WebM HTML5 Support, Flash Fallback For Other Content
+                "repeat": false,
+                "loop": false,
+                "stagevideo": false,
+                "stretching": "uniform",
+                "visualplaylist": false,
+                "width": window.innerWidth,
+                "height": window.innerHeight
+            });
+        }
     }
 });
