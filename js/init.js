f11df644c91c18b2d7e1dbe8b23bff9459f805b1
Júlio C. Oliveira <talk@juliocesar.me>
2023-06-12 10:27:43 -0300

    Clean up, code formatting, reuse of js functions shared replaced from manual to common.js

--- js/init.js
+++ js/init.js
@@ -1,24 +1,3 @@
-function get(variable)
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
-}
-
-function getByURL(variable, query)
-{
-       var vars = query.split("?");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
-}
-
 jwplayer.key = "lmwviL3c55Ymnx4fMjEUQeiU00zeXf6TCiDHQA==";
 
 // Get URL
@@ -26,51 +5,51 @@ var url = unescape(get("url"));
 
 // Youtube API Fallback
 
-window.onYouTubeIframeAPIReady = function() {
-	window.jwplayer = new YT.Player("player", {
-		"width": window.innerWidth,
-		"height": window.innerHeight,
-		videoId: getByURL("v", url),
-		playerVars: {
-			controls: 0,
-			autoplay: 1,
-			showinfo: 0,
-			iv_load_policy: 3,
-			autohide: 0,
-			rel: "0",
-			wmode: "opaque",
-			modestbranding: 1,
-			//nohtml5: 1
-		}
-	});
-	
-	jwplayer.pause = jwplayer.pauseVideo
-	jwplayer.play = jwplayer.playVideo
+window.onYouTubeIframeAPIReady = function () {
+    window.jwplayer = new YT.Player("player", {
+        "width": window.innerWidth,
+        "height": window.innerHeight,
+        videoId: getByURL("v", url),
+        playerVars: {
+            controls: 0,
+            autoplay: 1,
+            showinfo: 0,
+            iv_load_policy: 3,
+            autohide: 0,
+            rel: "0",
+            wmode: "opaque",
+            modestbranding: 1,
+            //nohtml5: 1
+        }
+    });
+
+    jwplayer.pause = jwplayer.pauseVideo
+    jwplayer.play = jwplayer.playVideo
 }
 
-$(document).ready(function () {	
-	// Check for URL
-	if (url != "false") {
-		// Uncomment For Youtube API Fallack
-		if (url.indexOf("youtube.com") === -1) {
-			/** Initialize player **/
-			jwplayer("player").setup({
-			  "aspectratio": "auto",
-			  "autostart": true,
-			  "controls": true,
-			  "displaydescription": false,
-			  "displaytitle": true,	    
-			  "file": url,
-			  "preload": "auto",
-			  "primary": "flash", // Use HTML5 As Primary To Have Native YouTube WebM HTML5 Support, Flash Fallback For Other Content
-			  "repeat": false,
-			  "loop": false,
-			  "stagevideo": false,
-			  "stretching": "uniform",
-			  "visualplaylist": false, 
-			  "width": window.innerWidth,
-			  "height": window.innerHeight
-			});
-		}
-	}
+$(document).ready(function () {
+    // Check for URL
+    if (url != "false") {
+        // Uncomment For Youtube API Fallack
+        if (url.indexOf("youtube.com") === -1) {
+            /** Initialize player **/
+            jwplayer("player").setup({
+                "aspectratio": "auto",
+                "autostart": true,
+                "controls": true,
+                "displaydescription": false,
+                "displaytitle": true,
+                "file": url,
+                "preload": "auto",
+                "primary": "flash", // Use HTML5 As Primary To Have Native YouTube WebM HTML5 Support, Flash Fallback For Other Content
+                "repeat": false,
+                "loop": false,
+                "stagevideo": false,
+                "stretching": "uniform",
+                "visualplaylist": false,
+                "width": window.innerWidth,
+                "height": window.innerHeight
+            });
+        }
+    }
 });
