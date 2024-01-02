5a47c4f67ce3a0188482b06fac5a8dbe2b644275
Júlio C. Oliveira <talk@juliocesar.me>
2024-01-02 00:22:09 -0300

    Updated copyright link

--- lua/autorun/client/cl_htmlcontrols.lua
+++ lua/autorun/client/cl_htmlcontrols.lua
@@ -1,7 +1,7 @@
 
 -- PlayX
 -- Copyright (c) 2009, 2010 sk89q <http://www.sk89q.com>
--- Copyright (c) 2011 - 2024 DathusBR <https://www.juliocesar.me>
+-- Copyright (c) 2011 - 2024 DathusBR <https://www.juliocesar.me> <http://steamcommunity.com/profiles/76561197983103320>
 --
 -- This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 -- To view a copy of this license, visit Common Creative's Website. <https://creativecommons.org/licenses/by-nc-sa/4.0/>
