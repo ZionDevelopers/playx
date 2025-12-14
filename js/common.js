2b53f3c50fea32ec2a8e4862ee30baf202426dbc
Júlio C. Oliveira <talk@juliocesar.me>
2025-12-14 10:13:42 -0300

    Update image provider

--- js/common.js
+++ js/common.js
@@ -39,3 +39,20 @@ function sendPlayerData(data) {
     }
     playx.processPlayerData(str);
 }
+
+ /**
+  * Conserve aspect ratio of the original region. Useful when shrinking/enlarging
+  * images to fit into a certain area.
+  *
+  * @param {Number} srcWidth width of source image
+  * @param {Number} srcHeight height of source image
+  * @param {Number} maxWidth maximum available width
+  * @param {Number} maxHeight maximum available height
+  * @return {Object} { width, height }
+  */
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+ }
