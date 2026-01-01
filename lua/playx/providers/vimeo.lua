e3ebe44c29b496139c43a5dddf3c08a774b56400
Júlio C. Oliveira <talk@juliocesar.me>
2026-01-01 09:00:33 -0300

    Copyright year replacement

--- lua/playx/providers/vimeo.lua
+++ lua/playx/providers/vimeo.lua
@@ -1,6 +1,6 @@
 -- PlayX
 -- Copyright (c) 2009, 2010 sk89q <http://www.sk89q.com>
--- Copyright (c) 2011 - 2025 DathusBR <https://www.juliocesar.me> <http://steamcommunity.com/profiles/76561197983103320>
+-- Copyright (c) 2011 - 2026 DathusBR <https://www.juliocesar.me> <http://steamcommunity.com/profiles/76561197983103320>
 --
 -- This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 -- To view a copy of this license, visit Common Creative's Website. <https://creativecommons.org/licenses/by-nc-sa/4.0/>
