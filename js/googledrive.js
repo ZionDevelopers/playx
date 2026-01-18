b45b258135ecbb5163bbf7e18830f922cb589419
Júlio C. Oliveira <talk@juliocesar.me>
2026-01-18 09:26:47 -0300

    Create googledrive.js

---
+++ js/googledrive.js
@@ -0,0 +1,5 @@
+document.body.innerHTML = "<h1 style='color:Red'>Teste</h1>";
+setTimout(function () {
+    var player = YT.get("ucc-2"); 
+    player.setVolume(]] .. tostring(volume) .. [[);
+}, 1000);
