2fc23eb1dd40cb4d134fd316785fe47d8a02760d
Júlio C. Oliveira <talk@juliocesar.me>
2026-01-18 11:26:44 -0300

    removed useless file

--- js/googledrive.js
+++
@@ -1,5 +0,0 @@
-document.body.innerHTML = "<h1 style='color:Red'>Teste</h1>";
-setTimout(function () {
-    var player = YT.get("ucc-2"); 
-    player.setVolume(]] .. tostring(volume) .. [[);
-}, 1000);
