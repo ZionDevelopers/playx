9e099d8997a0f551c809b4f1c2312e6da0b50fee
Júlio C. Oliveira <talk@juliocesar.me>
2026-07-07 13:17:41 -0300

    Removed old flash plugins

--- js/init.js
+++ js/init.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
             "displaytitle": true,
             "file": url,
             "preload": "auto",
-            "primary": "flash", // Use HTML5 As Primary To Have Native YouTube WebM HTML5 Support, Flash Fallback For Other Content
+            "primary": "html5", // Use HTML5 As Primary To Have Native YouTube WebM HTML5 Support, Flash Fallback For Other Content
             "repeat": false,
             "loop": false,
             "stagevideo": false,
